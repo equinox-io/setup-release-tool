0cf4cb3a8406f62a835f18fd7132e48594727416
Kyle Conroy <kyle@conroy.org>
2020-07-08 19:39:06 -0700

    Install version 1.14 of the Equinox release tool (#1)
    The release tool can be install on all three Runner platforms

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,39 @@ import * as tc from '@actions/tool-cache'
 
 async function run(): Promise<void> {
   try {
+    const toolDir = tc.find('equinox', '1.14.0', 'x64')
+    if (toolDir !== '') {
+      core.addPath(toolDir)
+      return
+    }
+
     switch(process.platform) {
       case 'win32': {
         const downloadPath = await tc.downloadTool('https://bin.equinox.io/a/3tDrUv1NjAT/release-tool-1.14.0-windows-amd64.zip')
         const extPath = await tc.extractZip(downloadPath)
-        core.debug(extPath);
+        const cachedPath = await tc.cacheDir(extPath, 'equinox', '1.14.0')
+        core.addPath(cachedPath)
         break;
       }
 
       case 'darwin': {
         const downloadPath = await tc.downloadTool('https://bin.equinox.io/a/dsR9Yc3Uxrc/release-tool-1.14.0-darwin-amd64.zip')
         const extPath = await tc.extractZip(downloadPath)
-        core.debug(extPath);
+        const cachedPath = await tc.cacheDir(extPath, 'equinox', '1.14.0')
+        core.addPath(cachedPath)
+        break;
+      }
+
+      case 'linux': {
+        const downloadPath = await tc.downloadTool('https://bin.equinox.io/a/hFqBgoEANbs/release-tool-1.14.0-linux-amd64.tar.gz')
+        const extPath = await tc.extractTar(downloadPath)
+        const cachedPath = await tc.cacheDir(extPath, 'equinox', '1.14.0')
+        core.addPath(cachedPath)
         break;
       }
 
       default: {
-        const downloadPath = await tc.downloadTool('https://bin.equinox.io/a/hFqBgoEANbs/release-tool-1.14.0-linux-amd64.tar.gz')
-        const extPath = await tc.extractTar(downloadPath)
-        core.debug(extPath);
-        break;
+        core.setFailed(`Unsupported platform: ${process.platform}`)
       }
     }
   } catch (error) {
